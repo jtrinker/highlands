947509fe58882e1113a4a67dc6c58bfb19ce0f66
Jameson Trinker <jameson.trinker@gmail.com>
2013-07-06 13:31:47 -0500

    root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ RedditOnRails::Application.routes.draw do
   devise_for :users
 
   resources :pages
-  root :to => "pages#index"
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
